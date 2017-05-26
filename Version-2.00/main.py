107c1165093b427860e5855ee5103aa74f14fbd6
algosuno <alejotomas8@hotmail.com>
2017-05-25 21:15:25 -0600

    ahora con las funciones de reproducir

---
+++ Version-2.00/main.py
@@ -0,0 +1,79 @@
+from archivo import Archivo
+from editor import Editor
+import cmd
+class Shell(cmd.Cmd, object):
+    intro='Bienvenido al reproductor, ? para ver comandos, help COMANDO para obtener ayuda'
+    prompt='*>>'
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.archivo=None
+        self.editor=None
+#-----------------------MANEJO DE ARCHIVOS---------------------------#
+    def do_REPRODUCIR(self,archivo):
+
+        self.archivo=Archivo(archivo)
+        self.archivo.leer()
+        self.archivo.conversion()
+        self.archivo.agregar_objeto()
+        self.archivo.reproducir()
+
+    def do_ABRIR(self,archivo):
+        self.archivo=Archivo(archivo)
+        self.archivo.leer()
+        self.archivo.conversion()
+        self.archivo.agregar_objeto()
+        self.editor=Editor(self.archivo)
+        self.editor.representar_cancion()
+
+    def do_STORE(self,archivo):
+        self.editor.guardar(archivo)
+#-----------------------MOVIMIENTO SOBRE TIEMPOS---------------------------#
+    def do_STEP(self,x):
+        self.editor.avanzar()
+        self.editor.representar_cancion()
+
+    def do_BACK(self,x):
+        self.editor.retroceder()
+        self.editor.representar_cancion()
+
+    def do_STEPM(self,x):
+        self.editor.avanzar(x)
+        self.editor.representar_cancion()
+
+    def do_BACKM(self,x):
+        editor.retroceder(x)
+        editor.representar_cancion()
+#-----------------------ADICION Y REMOCION DE SONIDOS---------------------------#
+    def do_TRACKADD(self,parametros):
+        linea=parametros.split()
+        tipo=linea[0]
+        frecuencia=linea[1]
+        volumen=linea[2]
+        self.editor.agregar_nota(tipo,volumen,frecuencia)
+        self.editor.representar_cancion()
+
+    def do_TRACKDEL(self,pos):
+        self.editor.quitar_nota(int(pos))
+        self.editor.representar_cancion()
+#-----------------------------REPRODUCCION---------------------------------#
+    def do_PLAYALL(self,x):
+        self.editor.reproducir()
+
+    def do_PLAY(self,x):
+        self.editor.reproducir_tiempos(1)
+
+    def do_PLAYMARKS(self,x):
+        self.editor.reproducir_tiempos(int(x))
+
+    def do_PLAYSECONDS(self,segundos):
+        self.editor.reproducir_tiempos(False,float(segundos))
+
+    def do_SALIR(self):
+        return 2//0
+
+
+
+
+
+
+Shell().cmdloop()
---
+++ Version-2.00/tiempo.py
@@ -0,0 +1,35 @@
+from ListaEnlazada import ListaEnlazada
+class Tiempo:
+    def __init__(self,duracion):
+        """Esto no es una documentacion, es solo un comentario sobre
+        el funcionamiento. La idea es que cada objeto tiempo tenga
+        como atributo, ademas de su duracion, una lista con los tiempos
+        en forma de True y False (que sirver para representar la cancion)
+        y una lista con los objetos a reproducir."""
+        self.duracion=duracion
+        self.notas=ListaEnlazada()
+        self.notas_obj=ListaEnlazada()
+#--------------------------------------------------------------------#
+    def agregar_nota(self,nota):
+        self.notas.append(nota)
+#--------------------------------------------------------------------#
+    def quitar_nota(self,pos):
+        self.notas.pop(pos)
+#--------------------------------------------------------------------#
+    def obtener_nota(self):
+        #Hay que implementar la clase iterador para porder convertir
+        # a la lista enlazada en una lista de python
+        return list(self.notas)
+#--------------------------------------------------------------------#
+    def agregar_nota_obj(self,nota):
+        self.notas_obj.append(nota)
+#--------------------------------------------------------------------#
+    def obtener_nota_obj(self):
+        #Hay que implementar la clase iterador para porder convertir
+        # a la lista enlazada en una lista de python
+        return list(self.notas_obj)
+#--------------------------------------------------------------------#
+    def obtener_tiempo(self):
+        return self.duracion
+    def __str__(self):
+        return 'Tiempo de duracion: {} segundos'.format(self.duracion)
