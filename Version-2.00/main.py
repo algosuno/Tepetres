fe6bfd07e4406bdd3c16ca8e0cf33b7ecdac4dc2
algosuno <alejotomas8@hotmail.com>
2017-05-29 12:09:19 -0600

    Update main.py

--- Version-2.00/main.py
+++ Version-2.00/main.py
@@ -1,16 +1,23 @@
 from archivo import Archivo
 from editor import Editor
 import cmd
+
+#--------------------------------------------------------------------#
+
 class Shell(cmd.Cmd, object):
+    '''Crea la interfaz del programa la cual manejara el usuario y va mostrando el progreso
+    de la edicion del archivo.'''
     intro='Bienvenido al reproductor, ? para ver comandos, help COMANDO para obtener ayuda'
     prompt='*>>'
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.archivo=None
         self.editor=None
+        
 #-----------------------MANEJO DE ARCHIVOS---------------------------#
-    def do_REPRODUCIR(self,archivo):
 
+    def do_REPRODUCIR(self,archivo):
+        '''Toma como parametro un archivo y lo reproduce'''
         self.archivo=Archivo(archivo)
         self.archivo.leer()
         self.archivo.conversion()
@@ -18,6 +25,7 @@ class Shell(cmd.Cmd, object):
         self.archivo.reproducir()
 
     def do_ABRIR(self,archivo):
+        '''Toma como parametro un archivo y lo abre en consola'''
         self.archivo=Archivo(archivo)
         self.archivo.leer()
         self.archivo.conversion()
@@ -26,25 +34,39 @@ class Shell(cmd.Cmd, object):
         self.editor.representar_cancion()
 
     def do_STORE(self,archivo):
+        '''Toma como parametro un archivo y lo guarda'''
         self.editor.guardar(archivo)
+        
 #-----------------------MOVIMIENTO SOBRE TIEMPOS---------------------------#
+
     def do_STEP(self,x):
+        '''Avanza una marca de tiempo'''
         self.editor.avanzar()
         self.editor.representar_cancion()
 
     def do_BACK(self,x):
+        '''Retrodece una marca de tiempo'''
         self.editor.retroceder()
         self.editor.representar_cancion()
 
     def do_STEPM(self,x):
+        '''Toma como parametro un numero 'x' y avanza 'x' marcas de tiempo.
+        Pre: 'x' en un numero int'''        
         self.editor.avanzar(x)
         self.editor.representar_cancion()
 
     def do_BACKM(self,x):
+        '''Toma como parametro un numero 'x' y retrocede 'x' marcas de tiempo.
+        Pre: 'x' en un numero int''' 
         editor.retroceder(x)
         editor.representar_cancion()
+        
 #-----------------------ADICION Y REMOCION DE SONIDOS---------------------------#
-    def do_TRACKADD(self,parametros):
+
+    def do_TRACKADD(self,parametros): #te parece mas descriptivo ponerle sonido al parametro?
+        '''Toma como parametro 'parametros' y agrega un nuevo track.
+        Pre: -'parametros' debe ser una lista que contenga tres partes
+             -'parametros' debe estar compuesta por digitos'''
         linea=parametros.split()
         tipo=linea[0]
         frecuencia=linea[1]
@@ -53,22 +75,33 @@ class Shell(cmd.Cmd, object):
         self.editor.representar_cancion()
 
     def do_TRACKDEL(self,pos):
+        '''Dada una posicion 'pos' elimina el track en dicha posicion'''
         self.editor.quitar_nota(int(pos))
         self.editor.representar_cancion()
+        
 #-----------------------------REPRODUCCION---------------------------------#
+
     def do_PLAYALL(self,x):
+        '''Reproduce todo el archivo desde el comienzo'''
         self.editor.reproducir()
 
     def do_PLAY(self,x):
+        '''Reproduce la marca en la que se encuentra el cursor actualmente''' #chequear desp
         self.editor.reproducir_tiempos(1)
 
     def do_PLAYMARKS(self,x):
+        '''Reproduce las marcas de tiempo dadas'''
         self.editor.reproducir_tiempos(int(x))
 
     def do_PLAYSECONDS(self,segundos):
+        '''Toma como parmetro una cantidad de segundos y los reproduce.
+        Pre: -'segundos' debe ser un numero'''
         self.editor.reproducir_tiempos(False,float(segundos))
 
     def do_SALIR(self):
+        '''Demasiado dificil de explicar en tan solo unas lineas. Se requiere
+        aprobar las materias: 67.01/68.09/61.23/64.05 y tener la tesis de ingenieria
+        nuclear'''
         return 2//0
 
 
